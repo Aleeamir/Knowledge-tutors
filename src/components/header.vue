9736c074ddfc7771877a683d55d95d58a84e1801
ali <aleeamir2001@gmail.com>
2024-01-16 23:12:52 +0500

    Initial commit

---
+++ src/components/header.vue
@@ -0,0 +1,159 @@
+<template>
+    <nav>
+        <div class="nav-inner">
+            <div class="logo">
+                <router-link to="/" class="logo-link">
+                    <img
+                        src="https://assets-global.website-files.com/636278799c5de66b6357762e/63a5ed608cfe91252bca83f1_TutorMe%20Logo.svg" />
+                </router-link>
+            </div>
+            <div class="inner-nav">
+                <RouterLink to="/">Who We Serve
+                </RouterLink>
+                <RouterLink to="/Accessories">
+                    Become A Tutor</RouterLink>
+                <RouterLink to="/Women">
+                    Resources
+                </RouterLink>
+                <RouterLink to="/Men">About
+                </RouterLink>
+            </div>
+            <div class="sidenav">
+                <RouterLink to="/About">Login</RouterLink>
+                <router-link to="/Accessories"><button class="button-1">Shop Now</button></router-link>
+            </div>
+        </div>
+    </nav>
+</template>
+<style scoped>
+.inner-nav a {
+    text-decoration: none;
+    color: black;
+    font-size: 20px;
+    font-family: 'Roboto', sans-serif;
+}
+
+.sidenav {
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+
+/* .nav-inner {
+    background-color: rgb(10, 10, 114);
+} */
+nav .nav-inner {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin: auto;
+}
+
+.logo {
+    padding: 20px;
+}
+
+.nav .nav-inner {
+    background-color: white;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+}
+
+* {
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.inner-nav a {
+    text-decoration: none;
+    color: black;
+    font-size: 20px;
+    font-family: 'Roboto', sans-serif;
+    padding: 0 15px;
+}
+
+.inner-nav {
+    border-right: black;
+}
+
+.sidenav a {
+    text-decoration: none;
+    color: black;
+    font-size: 20px;
+    font-family: 'Roboto', sans-serif;
+    padding-right: 35px;
+}
+
+.button-1 {
+    appearance: button;
+    background-color: #ecf0f1;
+    border: solid transparent;
+    border-radius: 16px;
+    border-width: 0 0 4px;
+    box-sizing: border-box;
+    color: #FFFFFF;
+    cursor: pointer;
+    display: inline-block;
+    font-family: din-round, sans-serif;
+    font-size: 15px;
+    font-weight: 700;
+    letter-spacing: .8px;
+    line-height: 20px;
+    margin: 0;
+    outline: none;
+    overflow: visible;
+    padding: 13px 1px;
+    text-align: center;
+    text-transform: uppercase;
+    touch-action: manipulation;
+    transform: translateZ(0);
+    transition: filter .2s;
+    user-select: none;
+    -webkit-user-select: none;
+    vertical-align: middle;
+    white-space: nowrap;
+    width: 100%;
+}
+
+.button-1:after {
+    background-clip: padding-box;
+    background-color: #81f188;
+    border: solid transparent;
+    border-radius: 6px;
+    border-width: 0 0 4px;
+    bottom: -4px;
+    content: "";
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    z-index: -1;
+}
+
+.button-1:main,
+.button-1:focus {
+    user-select: auto;
+}
+
+.button-1:hover:not(:disabled) {
+    filter: brightness(1.1);
+    -webkit-filter: brightness(1.1);
+}
+
+.button-1:disabled {
+    cursor: auto;
+}
+
+.button-1:active {
+    border-width: 4px 0 0;
+    background: none;
+}</style>  
---
+++ src/views/HomeView.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="home">
+    <div class="home-first">
+      <h1> Every student needs a </h1>
+      <p>{{ replacementWord }}.</p>
+      <router-link to="/"><button class="button-2">Why TutorMe?</button></router-link>
+      <div class="banner-1"><img
+          src="https://assets-global.website-files.com/636278799c5de66b6357762e/6425b8ac662f566f966232d4_superhero.png"
+          alt="">
+      </div>
+    </div>
+    <div class="home-second">
+      <h1>We take tutoring personally</h1>
+      <p>Every student’s success starts with a meaningful connection. We connect learners with the right tutors at the
+        right time, creating a ripple effect of better outcomes for the entire community.</p>
+
+    </div>
+  </div>
+
+  <!-- <main>
+    <TheWelcome />
+  </main> -->
+</template>
+<script>
+export default {
+  data() {
+    return {
+      replacementWord: 'mentor',
+      replacements: ['tutor', 'sidekick', 'cheerleader'],
+      currentIndex: 0,
+    };
+  },
+  mounted() {
+    this.startReplacementLoop();
+  },
+  beforeDestroy() {
+    clearInterval(this.intervalId);
+  },
+  methods: {
+    startReplacementLoop() {
+      this.intervalId = setInterval(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.replacements.length;
+        this.replacementWord = this.replacements[this.currentIndex];
+      }, 2000);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.home-first h1 {
+  text-align: center;
+  margin-top: 100px;
+  font-family: Ttcommons, sans-serif;
+  font-size: 6.375rem;
+  font-weight: 500;
+  line-height: 1;
+
+}
+
+.home-first p {
+  text-align: center;
+  /* margin-top: 20px; */
+  font-family: Ttcommons, sans-serif;
+  font-size: 6.375rem;
+  font-weight: 600;
+  line-height: 0;
+  font-style: italic;
+}
+
+.button-2 {
+  align-items: center;
+  background-color: initial;
+  background-color: #d4f559;
+  border-radius: 7px;
+  border-width: 0;
+  color: #0c0101;
+  cursor: pointer;
+  display: flex;
+  font-family: Quicksand, sans-serif;
+  font-size: 18px;
+  font-weight: 700;
+  justify-content: center;
+  letter-spacing: .04em;
+  line-height: 16px;
+  margin: 0;
+  padding: 18px 18px;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: baseline;
+  margin-left: 45%;
+}
+
+.banner-1 img {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  max-width: 1500px;
+  max-height: 500px;
+  margin-top: 80px;
+}
+
+.home-second {
+  display: flex;
+  flex-direction: column;
+  background-color: #1c8754;
+}
+
+.home-second h1 {
+  margin-bottom: 1rem;
+  font-family: Ivartext, sans-serif;
+  font-size: 3.375rem;
+  font-weight: 700;
+  line-height: 1.25;
+  text-align: center;
+  background-color: #1c8754;
+  color: white;
+
+
+}
+
+.home-second p {
+  margin-bottom: 1em;
+  font-size: 1.25rem;
+  font-weight: 400;
+  line-height: 1.3;
+  text-align: center;
+  background-color: #1c8754;
+  color: white;
+}
+
+/* .button-2:hover {
+  background-image: linear-gradient(#B384C9, #391F5B 50%);
+} */
+
+/* @media (min-width: 768px) {
+  .button-72 {
+    font-size: 21px;
+    padding: 18px 34px;
+  }
+} */</style>
